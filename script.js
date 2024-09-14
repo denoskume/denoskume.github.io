fbbf03210c2aa002c5dad8dae85b1fbe814091ca
Denos Kume <110854822+denoskume@users.noreply.github.com>
2024-09-14 14:12:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function loadContent(page) {
-    // Create an XMLHttpRequest to fetch the content dynamically
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function() {
-        // Insert the loaded content into the 'content' div
         document.getElementById("content").innerHTML = this.responseText;
     };
-    // Load the respective page (e.g., home.html, about.html, etc.)
     xhttp.open("GET", `${page}.html`, true);
     xhttp.send();
 }
 
+// Load Home content by default when the page is first loaded
+window.onload = function() {
+    loadContent('home'); // This will load 'home.html' by default
+};
